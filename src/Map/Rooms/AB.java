74e21dfdd9998c71221517f56a9c4658f2dac5e8
Gabriel Sousa <gabriel2001@live.com.pt>
2022-12-22 12:56:18 +0000

    structure of game

---
+++ src/Map/Rooms/AB.java
@@ -0,0 +1,8 @@
+package Map.Rooms;
+
+public class AB extends Room{
+    @Override
+    public void print() {
+        System.out.println("AB");
+    }
+}
---
+++ src/Map/Rooms/FA.java
@@ -0,0 +1,8 @@
+package Map.Rooms;
+
+public class FA extends Room{
+    @Override
+    public void print() {
+        System.out.println("FA");
+    }
+}
