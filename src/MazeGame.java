3e660ef38942a1b36fc9142590455222d989e416
FranciscoSilvaMgLPT <Francisco_silva2k7@hotmail.com>
2022-12-23 20:35:56 +0000

    TextImages

--- src/MazeGame.java
+++ src/MazeGame.java
@@ -3,4 +3,10 @@ public class MazeGame {
     public static void gameIntro(){
 
     }
+    public static void howToPlay(){
+        System.out.println("\n" +
+                "░█─░█ ░█▀▀▀█ ░█──░█ 　 ▀▀█▀▀ ░█▀▀▀█ 　 ░█▀▀█ ░█─── ─█▀▀█ ░█──░█ \n" +
+                "░█▀▀█ ░█──░█ ░█░█░█ 　 ─░█── ░█──░█ 　 ░█▄▄█ ░█─── ░█▄▄█ ░█▄▄▄█ \n" +
+                "░█─░█ ░█▄▄▄█ ░█▄▀▄█ 　 ─░█── ░█▄▄▄█ 　 ░█─── ░█▄▄█ ░█─░█ ──░█──");
+    }
 }
