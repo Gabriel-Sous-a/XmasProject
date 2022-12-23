b5f7215d07b6ea3dabc915aa8cb057f065a6ece9
FranciscoSilvaMgLPT <Francisco_silva2k7@hotmail.com>
2022-12-23 16:19:23 +0000

    Add: colors

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,9 @@
+import VisualStats.Colors;
 import VisualStats.TextArtImages;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(
-                " ______________\n" +
-                        "|\\ ___________ /|\n" +
-                        "| |  _ _ _ _  | |\n" +
-                        "| | | | | | | | |\n" +
-                        "| | |-+-+-+-| | |\n" +
-                        "| | |-+-+=+%| | |\n" +
-                        "| | |_|_|_|_| | |\n" +
-                        "| |    ___    | |\n" +
-                        "| |   [___] ()| |\n" +
-                        "| |         ||| |\n" +
-                        "| |         ()| |\n" +
-                        "| |           | |\n" +
-                        "| |           | |\n" +
-                        "| |           | |\n" +
-                        "|_|___________|_|"
-        );
-        TextArtImages.door();
+        System.out.println(Colors.BLACK_BACKGROUND);
 
     }
 }
